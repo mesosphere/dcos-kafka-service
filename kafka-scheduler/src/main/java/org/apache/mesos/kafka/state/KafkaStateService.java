d7858c7e191a2f36a5cf3c712229c6434c684a03
Gabriel Hartmann <gabriel@mesosphere.io>
2016-01-19 15:39:40 -0800

    TaskStatus updates persisted

--- kafka-scheduler/src/main/java/org/apache/mesos/kafka/state/KafkaStateService.java
+++ kafka-scheduler/src/main/java/org/apache/mesos/kafka/state/KafkaStateService.java
@@ -12,11 +12,17 @@ import org.apache.mesos.config.ConfigurationService;
 import org.apache.mesos.kafka.config.KafkaConfigService;
 
 import org.apache.mesos.Protos.FrameworkID;
+import org.apache.mesos.Protos.TaskID;
 import org.apache.mesos.Protos.TaskInfo;
+import org.apache.mesos.Protos.TaskState;
+import org.apache.mesos.Protos.TaskStatus;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
 
-public class KafkaStateService {
+public class KafkaStateService implements Observer {
   private final Log log = LogFactory.getLog(KafkaStateService.class);
   private final CuratorFramework zkClient;
   
@@ -29,6 +35,21 @@ public class KafkaStateService {
   private static String fwkIdPath;
 
   private static KafkaStateService stateService = null;
+
+  private KafkaStateService() {
+    zkClient = createCuratorClient();
+
+    zkRoot = "/" + config.get("FRAMEWORK_NAME") + "/state";
+    taskPath = zkRoot + "/tasks";
+    fwkIdPath = zkRoot + "/framework-id";
+
+    try {
+      initializePath(zkRoot + "/tasks");
+      initializePath(zkRoot + "/framework-id");
+    } catch(Exception ex) {
+      log.fatal("Failed with exception: " + ex);
+    }
+  }
 
   public static KafkaStateService getStateService() {
     if (stateService == null) {
@@ -61,25 +82,53 @@ public class KafkaStateService {
   }
 
   public void recordTasks(List<TaskInfo> taskInfos) throws Exception {
-    for (TaskInfo taskInfo : taskInfos) {
-      String infoPath = taskPath + "/" + taskInfo.getName();
-      zkClient.create().forPath(infoPath, taskInfo.toByteArray());
+    List<TaskStatus> taskStatuses = taskInfosToTaskStatuses(taskInfos); 
+    recordTaskStatuses(taskStatuses);
+  }
+
+  public void update(Observable observable, Object obj) {
+    TaskStatus taskStatus = (TaskStatus) obj;
+
+    try {
+      recordTaskStatus(taskStatus);
+    } catch (Exception ex) {
+      log.error("Failed to update TaskStatus: " + taskStatus + "with exception: " + ex);
     }
   }
 
-  private KafkaStateService() {
-    zkClient = createCuratorClient();
+  private void recordTaskStatus(TaskStatus taskStatus) throws Exception {
+    String statusPath = taskPath + "/" + taskStatus.getTaskId().getValue();
 
-    zkRoot = "/" + config.get("FRAMEWORK_NAME") + "/state";
-    taskPath = zkRoot + "/tasks";
-    fwkIdPath = zkRoot + "/framework-id";
-
-    try {
-      initializePath(zkRoot + "/tasks");
-      initializePath(zkRoot + "/framework-id");
-    } catch(Exception ex) {
-      log.fatal("Failed with exception: " + ex);
+    if (zkClient.checkExists().forPath(statusPath) == null) {
+      zkClient.create().forPath(statusPath, taskStatus.toByteArray());
+    } else {
+      zkClient.setData().forPath(statusPath, taskStatus.toByteArray());
     }
+  }
+
+  private void recordTaskStatuses(List<TaskStatus> taskStatuses) throws Exception {
+    for (TaskStatus taskStatus : taskStatuses) {
+      recordTaskStatus(taskStatus);
+    }
+  }
+
+  private TaskStatus taskInfoToTaskStatus(TaskInfo taskInfo) {
+      TaskID taskId = taskInfo.getTaskId();
+
+      TaskStatus.Builder taskBuilder = TaskStatus.newBuilder();
+      taskBuilder.setTaskId(taskId);
+      taskBuilder.setState(TaskState.TASK_STAGING);
+      return taskBuilder.build();
+  }
+
+  private List<TaskStatus> taskInfosToTaskStatuses(List<TaskInfo> taskInfos) {
+    List<TaskStatus> taskStatuses = new ArrayList<TaskStatus>();
+
+    for (TaskInfo taskInfo : taskInfos) {
+      taskStatuses.add(taskInfoToTaskStatus(taskInfo));
+    } 
+
+    return taskStatuses;
   }
 
   private void initializePath(String path) throws Exception {

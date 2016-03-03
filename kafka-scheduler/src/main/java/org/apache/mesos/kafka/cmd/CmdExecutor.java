89791c0a14c19957750c54b642c2049e56176c2e
Nick <nick@mesosphere.io>
2016-03-02 19:04:20 -0800

    Fix topic delete, add cmd logging

--- kafka-scheduler/src/main/java/org/apache/mesos/kafka/cmd/CmdExecutor.java
+++ kafka-scheduler/src/main/java/org/apache/mesos/kafka/cmd/CmdExecutor.java
@@ -11,6 +11,7 @@ import java.io.InputStreamReader;
 import javax.ws.rs.core.MultivaluedMap;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.StopWatch;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -28,7 +29,7 @@ public class CmdExecutor {
   private static KafkaStateService state = KafkaStateService.getStateService();
 
   private static String binPath = config.get("MESOS_SANDBOX") + "/" + config.get("KAFKA_VER_NAME") + "/bin/";
-  private static String zkPath = config.getKafkaZkUri(); 
+  private static String zkPath = config.getKafkaZkUri();
 
   public static JSONObject createTopic(String name, int partitionCount, int replicationFactor) throws Exception {
     // e.g. ./kafka-topics.sh --create --zookeeper master.mesos:2181/kafka-0 --topic topic0 --partitions 3 --replication-factor 3
@@ -45,7 +46,7 @@ public class CmdExecutor {
     cmd.add("--replication-factor");
     cmd.add(Integer.toString(replicationFactor));
 
-    return runCmd(cmd); 
+    return runCmd(cmd);
   }
 
   public static JSONObject deleteTopic(String name) throws Exception {
@@ -54,12 +55,12 @@ public class CmdExecutor {
     List<String> cmd = new ArrayList<String>();
     cmd.add(binPath + "kafka-topics.sh");
     cmd.add("--delete");
-    cmd.add("--zookeeper ");
+    cmd.add("--zookeeper");
     cmd.add(zkPath);
     cmd.add("--topic ");
     cmd.add(name);
 
-    return runCmd(cmd); 
+    return runCmd(cmd);
   }
 
   public static JSONObject alterTopic(String name, List<String> cmds) throws Exception {
@@ -74,7 +75,7 @@ public class CmdExecutor {
     cmd.add(name);
     cmd.addAll(cmds);
 
-    return runCmd(cmd); 
+    return runCmd(cmd);
   }
 
   public static List<String> getListOverrides(MultivaluedMap<String, String> overrides) {
@@ -91,7 +92,7 @@ public class CmdExecutor {
   public static JSONObject producerTest(String topicName, int messages) throws Exception {
     // e.g. ./kafka-producer-perf-test.sh --topic topic0 --num-records 1000 --producer-props bootstrap.servers=ip-10-0-2-171.us-west-2.compute.internal:9092,ip-10-0-2-172.us-west-2.compute.internal:9093,ip-10-0-2-173.us-west-2.compute.internal:9094 --throughput 100000 --record-size 1024
     List<String> brokerEndpoints = state.getBrokerEndpoints();
-    String brokers = StringUtils.join(brokerEndpoints, ","); 
+    String brokers = StringUtils.join(brokerEndpoints, ",");
     String bootstrapServers = "bootstrap.servers=" + brokers;
 
     List<String> cmd = new ArrayList<String>();
@@ -107,14 +108,14 @@ public class CmdExecutor {
     cmd.add("--producer-props");
     cmd.add(bootstrapServers);
 
-    return runCmd(cmd); 
+    return runCmd(cmd);
   }
 
   public static JSONArray getOffsets(String topicName) throws Exception {
     // e.g. ./kafka-run-class.sh kafka.tools.GetOffsetShell --broker-list ip-10-0-1-71.us-west-2.compute.internal:9092,ip-10-0-1-72.us-west-2.compute.internal:9093,ip-10-0-1-68.us-west-2.compute.internal:9094 --topic topic0 --time -1 --partitions 0
 
     List<String> brokerEndpoints = state.getBrokerEndpoints();
-    String brokers = StringUtils.join(brokerEndpoints, ","); 
+    String brokers = StringUtils.join(brokerEndpoints, ",");
 
     List<String> cmd = new ArrayList<String>();
     cmd.add(binPath + "kafka-run-class.sh");
@@ -173,13 +174,29 @@ public class CmdExecutor {
     return new JSONArray(partitions);
   }
 
-  private static JSONObject runCmd(List<String> cmd) throws Exception{
+  private static JSONObject runCmd(List<String> cmd) throws Exception {
     ProcessBuilder builder = new ProcessBuilder(cmd);
+
+    StopWatch stopWatch = new StopWatch();
+    stopWatch.start();
     Process process = builder.start();
     int exitCode = process.waitFor();
+    stopWatch.stop();
 
     String stdout = streamToString(process.getInputStream());
     String stderr = streamToString(process.getErrorStream());
+
+    if (exitCode == 0) {
+      log.info(String.format(
+          "Command succeeded in %dms: %s",
+          stopWatch.getTime(), StringUtils.join(cmd, " ")));
+    } else {
+      log.warn(String.format(
+          "Command failed with code=%d in %dms: %s",
+          exitCode, stopWatch.getTime(), StringUtils.join(cmd, " ")));
+      log.warn(String.format("stdout:\n%s", stdout));
+      log.warn(String.format("stderr:\n%s", stderr));
+    }
 
     JSONObject obj = new JSONObject();
     obj.put("stdout", stdout);

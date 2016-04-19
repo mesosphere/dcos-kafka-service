49d2e67fe9d5ff8720d460ea2ceffe7cceeabaa0
Nick <nick@mesosphere.io>
2016-04-18 18:20:59 -0700

    Fix auth handling and broker lookup in sample clients
    - Update broker lookup to reflect new keys
    - Use internal endpoints to avoid triggering auth
    - Update launcher script to support passing in raw DC/OS tokens

--- kafka-test-client/src/main/java/org/apache/mesos/kafka/testclient/BrokerLookup.java
+++ kafka-test-client/src/main/java/org/apache/mesos/kafka/testclient/BrokerLookup.java
@@ -12,14 +12,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.net.URLConnection;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BrokerLookup {
   private static final Logger LOGGER = LoggerFactory.getLogger(BrokerLookup.class);
-  private static final String PATH_TEMPLATE = "service/%s/v1/connection";
+  private static final String MARATHON_LOOKUP_TEMPLATE = "http://master.mesos:8080/v2/apps/%s";
+  private static final String SCHEDULER_CONNECTION_TEMPLATE = "http://%s.mesos:%d/v1/connection";
 
   private BrokerLookupConfig config;
 
@@ -28,59 +28,26 @@ public class BrokerLookup {
   }
 
   public List<String> getBootstrapServers() throws IOException {
-    URL url;
-    BufferedReader reader = null;
-    JSONObject responseObj;
 
-    try {
-      if (config.masterHost.startsWith("http")) {
-        // assume value is of form 'http://master.mesos[/]'
-        StringBuilder urlBuilder = new StringBuilder().append(config.masterHost);
-        if (!config.masterHost.endsWith("/")) {
-          urlBuilder.append('/');
-        }
-        urlBuilder.append(String.format(PATH_TEMPLATE, config.frameworkName));
-        url = new URL(urlBuilder.toString());
-      } else {
-        // assume config is of form 'master.mesos'
-        url = new URL(
-          "http",
-          config.masterHost,
-          String.format("/" + PATH_TEMPLATE, config.frameworkName));
-      }
-      LOGGER.info("Connecting to {} for broker lookup", url.toString());
+    // First, query marathon to get the scheduler's port
+    // (too lazy to figure out SRV record lookup in Java)
+    URL url = new URL(String.format(MARATHON_LOOKUP_TEMPLATE, config.frameworkName));
+    LOGGER.info("Connecting to {} for scheduler port lookup", url.toString());
+    JSONObject responseObj = getAndLogResponse(url);
 
-      if (!config.frameworkAuthToken.isEmpty()) {
-        // Include provided authtoken in request headers
-        URLConnection connection = url.openConnection();
-        connection.setRequestProperty("Authorization", "token=" + config.frameworkAuthToken);
-        connection.setDoOutput(true);
-      }
+    // response["app"]["tasks"][0]["ports"][0] = portnum
+    int schedulerPort = responseObj
+        .getJSONObject("app")
+        .getJSONArray("tasks")
+        .getJSONObject(0)
+        .getJSONArray("ports")
+        .getInt(0);
 
-      if (LOGGER.isInfoEnabled()) {
-        // Intercept the response text so that it can be logged
-        reader = new BufferedReader(new InputStreamReader(url.openStream(), Charset.defaultCharset()));
-        StringBuilder responseBuilder = new StringBuilder();
-        while (true) {
-          String line = reader.readLine();
-          if (line == null) {
-            break;
-          }
-          responseBuilder.append(line).append('\n');
-        }
-        LOGGER.info("Got response: {}", responseBuilder.toString().trim());
-        responseObj = new JSONObject(responseBuilder.toString());
-        LOGGER.info("JSON from response: {}", responseObj.toString());
-      } else {
-        // Pass response stream directly to JSONTokener, foregoing access to response content
-        responseObj = new JSONObject(new JSONTokener(url.openStream()));
-      }
-    } finally {
-      IOUtils.closeQuietly(reader);
-    }
+    url = new URL(String.format(SCHEDULER_CONNECTION_TEMPLATE, config.frameworkName, schedulerPort));
+    LOGGER.info("Connecting to {} for broker lookup", url.toString());
+    responseObj = getAndLogResponse(url);
 
-
-    JSONArray responseBrokerList = responseObj.getJSONArray("brokers");
+    JSONArray responseBrokerList = responseObj.getJSONArray("dns");
     List<String> brokerList = new ArrayList<>();
     for (int i = 0; i < responseBrokerList.length(); ++i) {
       brokerList.add(responseBrokerList.getString(i));
@@ -89,5 +56,33 @@ public class BrokerLookup {
       LOGGER.warn("Response from {} contained an empty broker list", url.toString());
     }
     return brokerList;
+  }
+
+  private JSONObject getAndLogResponse(URL url) throws IOException {
+    if (!LOGGER.isInfoEnabled()) {
+      // Pass response stream directly to JSONTokener, foregoing access to response content
+      return new JSONObject(new JSONTokener(url.openStream()));
+    }
+
+    // Intercept the response text so that it can be logged
+    BufferedReader reader = null;
+    try {
+      reader = new BufferedReader(new InputStreamReader(url.openStream(), Charset.defaultCharset()));
+      StringBuilder responseBuilder = new StringBuilder();
+      while (true) {
+        String line = reader.readLine();
+        if (line == null) {
+          break;
+        }
+        responseBuilder.append(line).append('\n');
+      }
+      String response = responseBuilder.toString();
+      LOGGER.info("Got response: {}", response.trim());
+      JSONObject jsonObject = new JSONObject(response);
+      LOGGER.info("JSON from response: {}", jsonObject.toString());
+      return jsonObject;
+    } finally {
+      IOUtils.closeQuietly(reader);
+    }
   }
 }

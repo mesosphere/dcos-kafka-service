a55e327b22389188e33d2fec56a10ea642092630
Nick <nick@mesosphere.io>
2016-08-16 16:23:08 -0700

    Switch to tools.tgz

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-
-# This script does a full build/upload of Kafka artifacts.
-# This script is invoked by Jenkins CI, as well as by build-docker.sh.
+#!/usr/bin/env bash
 
 # Prevent jenkins from immediately killing the script when a step fails, allowing us to notify github:
 set +e
@@ -9,25 +6,12 @@ set +e
 REPO_ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $REPO_ROOT_DIR
 
-if [ -z "$AWS_ACCESS_KEY_ID" -o -z "$AWS_SECRET_ACCESS_KEY" ]; then
-    echo "Missing required AWS access info: AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY"
-    exit 1
-fi
-
-# In theory, we could use Jenkins' "Multi SCM" script, but good luck with getting auto-build to work with that
-# Instead, clone the secondary 'dcos-commons' repo manually.
-if [ -d dcos-commons ]; then
-  cd dcos-commons/
-  git pull origin master
-  cd ..
-else
-  git clone --depth 1 --branch master git@github.com:mesosphere/dcos-commons.git
-fi
-echo Running with dcos-commons tools rev: $(git --git-dir=dcos-commons/.git rev-parse HEAD)
+# Grab dcos-commons build/release tools:
+rm -rf dcos-commons-tools/ && curl https://infinity-artifacts.s3.amazonaws.com/dcos-commons-tools.tgz | tar xz
 
 # GitHub notifier config
 _notify_github() {
-    $REPO_ROOT_DIR/dcos-commons/tools/github_update.py $1 build $2
+    $REPO_ROOT_DIR/dcos-commons-tools/github_update.py $1 build $2
 }
 
 # Build steps for Kafka
@@ -52,14 +36,14 @@ fi
 
 cd cli/ && ./build-cli.sh
 if [ $? -ne 0 ]; then
-    _notify_github failure "CLI build failed"
-    exit 1
+  _notify_github failure "CLI build failed"
+  exit 1
 fi
 cd $REPO_ROOT_DIR
 
 _notify_github success "Build succeeded"
 
-./dcos-commons/tools/ci-upload.sh \
+./dcos-commons-tools/ci_upload.py \
   kafka \
   universe/ \
   kafka-scheduler/build/distributions/*.zip \

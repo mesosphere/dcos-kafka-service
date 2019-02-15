7858aa7e21f19359c4e87fcdc9f0dc1fff9e819d
Zain Malik <zmalikshxil@gmail.com>
2019-02-15 13:00:49 +0100

    fix the test_overlay issue with get_agents

--- frameworks/kafka/tests/utils/kafka_networks.py
+++ frameworks/kafka/tests/utils/kafka_networks.py
@@ -9,13 +9,14 @@ import sdk_agents
 def check_endpoints_on_overlay(endpoints):
     def check_ip_addresses_on_overlay():
         # the overlay IP address should not contain any agent IPs
-        return len(set(ip_addresses).intersection(set(sdk_agents.get_agents()))) == 0
+        return len(set(ip_addresses).intersection(set(all_agent_ips))) == 0
 
     assert "address" in endpoints, "endpoints: {} missing 'address' key".format(endpoints)
     assert "dns" in endpoints, "endpoints: {} missing 'dns' key".format(endpoints)
 
     # endpoints should have the format <ip_address>:port
     ip_addresses = [e.split(":")[0] for e in endpoints["address"]]
+    all_agent_ips = [agent["hostname"] for agent in sdk_agents.get_agents()]
     assert (
         check_ip_addresses_on_overlay()
     ), "IP addresses for this service should not contain agent IPs, IPs were {}".format(

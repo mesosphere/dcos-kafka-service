1cdc715c83d55925ad82121e618db04a031bfd95
Shubhanil Bag <shubhanil.bag@gmail.com>
2019-09-25 21:51:58 +0530

    Docker Image cleanup (#449)

--- images/scripts/start-kafka.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-${KAFKA_HOME}/generate-configuration.sh > ${KAFKA_HOME}/server.properties
-
-echo "starting the kafka broker using broker.id ${HOSTNAME##*-}..."
-KAFKA_OPTS="${KAFKA_OPTS} ${METRICS_OPTS}" exec ${KAFKA_HOME}/bin/kafka-server-start.sh ${KAFKA_HOME}/server.properties
